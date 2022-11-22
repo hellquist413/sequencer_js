51e691e3e50cf72ee126986e0d522eb9b9716dfa
hellquist413 <hellquistmusic@gmail.com>
2022-11-22 20:15:37 +0100

    Update createLink.js

--- js/createLink.js
+++ js/createLink.js
@@ -1,17 +1,21 @@
 async function createShortIO(urlToShorten) {
     console.log(urlToShorten);
-    const options = {
-        method: 'POST',
-        headers: {
-            accept: 'application/json',
-            'content-type': 'application/json',
-            authorization: 'pk_Fyb6rXgT61WeumwC'
-        },
-        body: JSON.stringify({ originalURL: urlToShorten, domain: "5sc4.short.gy"})
-    };
 
-    fetch('/api/shortIO', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    const options = {
+      method: 'POST',
+      url: '/api/shortIO',
+      headers: {
+        authorization: 'pk_Fyb6rXgT61WeumwC',
+      },
+      json: {
+        originalURL: urlToShorten,
+        domain: '5sc4.short.gy'
+      },
+      responseType: 'json'
+    };
+    
+    got(options).then(response => {
+      console.log(response.body);
+    });    
+
 }
