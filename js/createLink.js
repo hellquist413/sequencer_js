bd274ab8d987f75814347cc5c731a282616ef013
hellquist413 <hellquistmusic@gmail.com>
2022-11-22 18:33:26 +0100

    Update createLink.js

--- js/createLink.js
+++ js/createLink.js
@@ -1,15 +1,23 @@
 function createShortIO(urlToShorten) {
+    const CORS_HEADERS = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers':
+            'Origin, X-Requested-With, Content-Type, Accept',
+    }
     const options = {
         method: 'POST',
         headers: {
-          accept: 'application/json',
-          'content-type': 'application/json',
-          Authorization: 'pk_Fyb6rXgT61WeumwC'
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers':
+            'Origin, X-Requested-With, Content-Type, Accept',
+            accept: 'application/json',
+            'content-type': 'application/json',
+            Authorization: 'pk_Fyb6rXgT61WeumwC'
         },
-        body: JSON.stringify({originalURL: urlToShorten})
-      };
-      
-      fetch('https://api.short.io/links/public', options)
+        body: JSON.stringify({ originalURL: urlToShorten })
+    };
+
+    fetch('https://api.short.io/links/public', options)
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));
