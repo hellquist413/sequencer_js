ebb546ad14dbbd5940eb35452b3e62cbfb54bed2
hellquist413 <hellquistmusic@gmail.com>
2022-11-23 20:29:16 +0100

    Update createLink.js

--- js/createLink.js
+++ js/createLink.js
@@ -17,7 +17,6 @@ async function createShortIO(urlToShorten) {
     shortIOresponse = data.shortURL;
     console.log(shortIOresponse);
     navigator.clipboard.writeText(shortIOresponse);
-    window.location.assign(shortIOresponse);
     return shortIOresponse;
   })
 }
