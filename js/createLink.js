ec7a8b98fe848132d9bd94f4213b62a656a8cc11
hellquist413 <hellquistmusic@gmail.com>
2022-11-23 18:52:33 +0100

    Update createLink.js

--- js/createLink.js
+++ js/createLink.js
@@ -1,20 +1,9 @@
 function createShortIO(urlToShorten) {
     console.log(urlToShorten);
 
-    const response = fetch("/api/tiny", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: "hellqx"
-        },
-        
-        body: JSON.stringify({
-            "url": urlToShorten,
-            "domain": "tiny.one",
-            "alias": "preset",
-            "tags": "preset,link",
-            "expires_at": "2023-10-25 10:11:12"
-          })
+    shortIOquery = `https://api.short.cm/links/tweetbot?domain=5sc4.short.gy&apiKey=sk_PXVEvB8Biq7GcG7C&originalURL=` + urlToShorten;
+
+    const response = fetch(shortIOquery, {
     }).then(response => {
       console.log(response.json);
     });       
