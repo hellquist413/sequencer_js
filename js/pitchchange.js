98f05892fa1b55166f1fa44281357889cc9e0984
hellquist413 <hellquistmusic@gmail.com>
2022-11-26 00:23:50 +0100

    Update pitchchange.js

--- js/pitchchange.js
+++ js/pitchchange.js
@@ -2,31 +2,31 @@ function pitchChange(instId) {
     let getValue = document.getElementById("pitchKnob_" + instId).value;
     switch (instId) {
         case 0:
-            notes[0] = Tone.Midi(getValue);
+            notes[0] = getValue;
             break;
         case 1:
-            notes[1] = Tone.Midi(getValue);
+            notes[1] = getValue;
             break;
         case 2:
-            notes[2] = Tone.Midi(getValue);
+            notes[2] = getValue;
             break;
         case 3:
-            notes[3] = Tone.Midi(getValue);
+            notes[3] = getValue;
             break;
         case 4:
-            notes[4] = Tone.Midi(getValue);
+            notes[4] = getValue;
             break;
         case 5:
-            notes[5] = Tone.Midi(getValue);
+            notes[5] = getValue;
             break;
         case 6:
-            notes[6] = Tone.Midi(getValue);
+            notes[6] = getValue;
             break;
         case 7:
-            notes[7] = Tone.Midi(getValue);
+            notes[7] = getValue;
             break;
         case 8:
-            notes[8] = Tone.Midi(getValue);
+            notes[8] = getValue;
             break;
     }
 }
@@ -41,35 +41,35 @@ function updatePitchKnobs(knobId, pitchValue) {
             break;
         case 1:
             notes[1] = pitchValue;
-            getKnob.value = Tone.Midi(pitchValue);
+            getKnob.value = pitchValue;
             break;
         case 2:
             notes[2] = pitchValue;
-            getKnob.value = Tone.Midi(pitchValue);
+            getKnob.value = pitchValue;
             break;
         case 3:
             notes[3] = pitchValue;
-            getKnob.value = Tone.Midi(pitchValue);
+            getKnob.value = pitchValue;
             break;
         case 4:
             notes[4] = pitchValue;
-            getKnob.value = Tone.Midi(pitchValue);
+            getKnob.value = pitchValue;
             break;
         case 5:
             notes[5] = pitchValue;
-            getKnob.value = Tone.Midi(pitchValue);
+            getKnob.value = pitchValue;
             break;
         case 6:
             notes[6] = pitchValue;
-            getKnob.value = Tone.Midi(pitchValue);
+            getKnob.value = pitchValue;
             break;
         case 7:
             notes[7] = pitchValue;
-            getKnob.value = Tone.Midi(pitchValue);
+            getKnob.value = pitchValue;
             break;
         case 8:
             notes[8] = pitchValue;
-            getKnob.value = Tone.Midi(pitchValue);
+            getKnob.value = pitchValue;
             break;
     }
 
